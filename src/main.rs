66e2579d31f933328f9de990bc2494e3b1cc1952
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-01-10 20:49:59 +0100

    Add data fetching and display functionality for astronauts in space

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,41 @@ use colored::*;
 use figlet_rs::FIGfont;
 use serde::Deserialize;
 
+// * URL to fetch the data
 static URL: &'static str = "http://api.open-notify.org/astros.json";
 
+// * Struct to deserialize the JSON data
 #[derive(Debug, Deserialize)]
 struct People {
     craft: String,
     name: String,
 }
 
+// * Struct to deserialize the JSON data
 #[derive(Debug, Deserialize)]
 struct Inhabitant {
     people: Vec<People>,
     number: i32,
 }
 
+// * Display the header
 fn display_header() {
     let font = FIGfont::standard().unwrap();
+
     let figure = font.convert("Now In Space");
+
     println!("{}", figure.unwrap().to_string().blue());
     println!("---------------------------------");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // * Fetch the data from the URL
     let client = reqwest::Client::new();
+    // * Send the request
     let res = client.get(URL.to_owned()).send().await?;
 
+    // * Deserialize the JSON data
     let people: Inhabitant = res.json().await?;
 
     display_header();
@@ -36,6 +45,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("---------------------------------");
 
+    // * Display the people in space
     people.people.iter().for_each(|p| {
         println!("{} is in the {} craft", p.name.cyan(), p.craft.red());
     });
